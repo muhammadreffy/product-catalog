c95c3b63cc6ec26f4e4d0a6e388891c89622196a
Muhammad Reffy <syahnataa@gmail.com>
2025-03-11 11:04:42 +0700

    close navbar menu when navItems on click

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const header = document.querySelector(".header");
 const navMenu = document.querySelector(".nav__menu");
+const navItems = document.querySelectorAll(".nav__item");
 
 hamburgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("nav__menu--active");
+});
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navMenu.classList.remove("nav__menu--active");
+  });
 });
 
 window.addEventListener("scroll", () => {
